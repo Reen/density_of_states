e88029cb22e38295fdc044af3d485de2e606c19c
René Haber <rene@habr.de>
2013-08-20 21:23:11 +0200

    Implemented transition matrix sampler.

--- src/main.cpp
+++ src/main.cpp
@@ -415,6 +415,42 @@ public:
 	}
 };
 
+class TransitionMatrixSampler : public MCSampler {
+private:
+	const matrix_int_t& Q;
+	vector_int_t column_sum;
+public:
+	TransitionMatrixSampler(boost::mt19937 &rng, size_t ms, double, double, double, const matrix_int_t& qmat)
+		: MCSampler(rng, ms), Q(qmat), column_sum(qmat.size1(),0) {
+	}
+	bool operator()(const int &E_old, const int &E_new) {
+		column_sum[E_old]++;
+#ifndef NDEBUG
+		//int Hold_sum(0), Hnew_sum(0);
+		//for (size_t i = 0; i < Q.size1(); i++) {
+			//Hold_sum += Q(E_old,i);
+			//Hnew_sum += Q(E_new,i);
+		//}
+		//assert(column_sum[E_old]==Hold_sum);
+		//assert(column_sum[E_new]==Hnew_sum);
+#endif
+		if (column_sum[E_new] == 0 || column_sum[E_old] == 0) {
+			return true;
+		}
+		double Tji, Tij;
+		Tji = (double)Q(E_new, E_old) / column_sum[E_new];
+		Tij = (double)Q(E_old, E_new) / column_sum[E_old];
+		bool res = ((Tji >= Tij) || (dist01(rng) <= (Tji / Tij)));
+		//std::cout << __LINE__ << " " << res << " " << Tji << " " << Tij << std::endl;
+		return res;
+	}
+
+	void check(const size_t &step, const size_t &run) {}
+
+	bool has_own_statistics() {
+		return false;
+	}
+};
 
 
 class Simulation {
@@ -788,6 +824,7 @@ public:
 		sampler_string.push_back("WL");
 		sampler_string.push_back("QB");
 		sampler_string.push_back("QA");
+		sampler_string.push_back("TM");
 
 		// determin path to the "graph" executable
 		// to add more OS see:
@@ -862,6 +899,9 @@ public:
 			break;
 		case 3:
 			mc_loop<QualityMeasureASampler>();
+			break;
+		case 4:
+			mc_loop<TransitionMatrixSampler>();
 			break;
 		default:
 			std::cerr << "Error: unknown sampler" << std::endl;
