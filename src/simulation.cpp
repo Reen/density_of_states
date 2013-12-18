4026ee04c32eff42633cd0561b7731ee6ba34388
René Haber <rene@habr.de>
2013-12-18 11:48:55 +0100

    Added help text for 1/t.

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -55,7 +55,7 @@ void Simulation::parse_arguments(int argc, char *argv[]) {
 		) (
 			"sampler",
 			po::value<size_t>()->default_value(1),
-			"Sampler to use:\n0 – Boltzmann, 1 – Wang-Landau, 2 – Q-Matrix B, 3 – Q-Matrix A, 4 – TransitionMatrix"
+			"Sampler to use:\n0 – Boltzmann, 1 – Wang-Landau, 2 – Q-Matrix B, 3 – Q-Matrix A, 4 – TransitionMatrix, 5 – 1/t Algorithm"
 		) (
 			"flatness,f",
 			po::value<double>()->default_value(0.99),
