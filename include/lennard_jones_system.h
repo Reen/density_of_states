f862e8db0da23870f2635bfff2c549ccd3ac8a57
René Haber <rene@habr.de>
2014-02-28 13:56:14 +0100

    Formatting.

--- include/lennard_jones_system.h
+++ include/lennard_jones_system.h
@@ -214,7 +214,8 @@ private:
           double err_lq, err_gth, err_power;
           bool   suc_lq, suc_gth, suc_power;
           double q_error;
-          boost::tie(err_lq, err_gth, err_power, suc_lq, suc_gth, suc_power, q_error) = rhab::calculate_error_q(dos_exact_norm, matrix_double_t(), Q, Qd, error_matrices, index);
+          boost::tie(err_lq, err_gth, err_power, suc_lq, suc_gth, suc_power, q_error) =
+            rhab::calculate_error_q(dos_exact_norm, matrix_double_t(), Q, Qd, error_matrices, index);
           if (suc_lq) {
             error_acc[index].err1(err_lq);
           }
