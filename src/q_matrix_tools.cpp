7db3ed87a78f9048cebd8f77d95b211f4696c81e
René Haber <rene@habr.de>
2014-03-22 14:26:17 +0100

    Eased power method success criterion

--- src/q_matrix_tools.cpp
+++ src/q_matrix_tools.cpp
@@ -325,7 +325,7 @@ bool rhab::calculate_dos_power(const matrix_double_t &imat, vector_double_t &t1)
   } while(!iter.converged(t2.begin(), t2.end(), t1.begin(), dist));
 
   size_t cnt_zero = std::count_if(t2.begin(), t2.end(), std::bind2nd(std::equal_to<double>(), 0.0));
-  bool enought_non_zero   = ( cnt_zero < 0.2 * t2.size() );
+  bool enought_non_zero   = ( cnt_zero < 0.7 * t2.size() );
   bool all_entries_finite = ( std::count_if(t2.begin(), t2.end(),
         boost::math::isfinite<double>) == (long)t2.size() );
   return ( enought_non_zero && all_entries_finite );
